fa7ca0e7d935311a7ca21d327f352ba12eaa6803
Oskari Mäkinen <jaakko.makinen@aalto.fi>
2019-04-24 19:57:56 +0300

    Implemented some basic tests

--- src/atcgame/tests/Tests.scala
+++ src/atcgame/tests/Tests.scala
@@ -11,8 +11,28 @@ import atcgame._
 @RunWith(classOf[JUnitRunner])
 class ATCTest extends FlatSpec with Matchers {
   
-  "Game checkCollisions" should "return false when no planes overlap" in {
-    //val game = new Game()
+  "Game checkCollisions" should "properly detect collisions" in {
+    
+    val g = new Game()
+    
+    val plane1 = new Plane(g, "")
+    val plane2 = new Plane(g, "")
+    val plane3 = new Plane(g, "")
+    
+    plane1.x = 10; plane1.y = 10
+    plane2.x = 100; plane2.y = 100
+    plane3.x = 200; plane3.y = 200
+    
+    g.planes ++= Vector(plane1, plane2, plane3)
+    
+    val collision = g.checkCollisions()
+    assert(collision == false, "collisions reported although no planes overlap")
+    
+    plane2.x = 50; plane2.y = 50
+    plane3.x = 80; plane3.y = 80
+    
+    val collision2 = g.checkCollisions()
+    assert(collision2, "collision not detected although planes overlap")
     
   }
 }
